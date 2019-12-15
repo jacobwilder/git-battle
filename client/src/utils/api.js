ae4f1908592cd5c3a6ff313241202db7416e714c
loljoho <loljoho@users.noreply.github.com>
2019-12-15 15:24:07 -0500

    Update React axios calls

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,24 +1,24 @@
 import axios from 'axios';
 
 export const getProfile = username => {
-  return axios.get(`/api/${username}`);
+  return axios.get(`/api/user/${username}`);
 };
 
 export const getUserData = (username1,username2) => {
     console.log(username1, username2);
-    return axios.get(`/api/${username1}/${username2}`);
+    return axios.get(`/api/user/${username1}/${username2}`);
 };
 
 export const getUserRepos = username => {
-  return axios.get(`/api/repos/${username}`);
+  return axios.get(`/api/user/repos/${username}`);
 };
 
 export const postUserData = userData => {
-  return axios.post(`/data/scoreboard`, userData);
+  return axios.post(`/api/data/scoreboard`, userData);
 };
 
 export const findUserData = userData => {
-  return axios.get(`/data/scoreboard`, userData);
+  return axios.get(`/api/data/scoreboard`, userData);
 };
 
 
